2d54dbd9c3df2a18851d89fa181df31e5102fa1b
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-02 15:24:08 -0400

    feat: show adjustable gradient

--- ProfExercise02.py
+++ ProfExercise02.py
@@ -8,10 +8,19 @@ def main():
     key = -1
     ESC_KEY = 27
     
+    max_gray = 100
+    
     while key != ESC_KEY:
-        
+        for i in range(gradient.shape[1]):
+            fraction = i / (gradient.shape[1]-1)
+            gradient[:,i:(i+1)] = fraction*max_gray
+                    
         cv2.imshow("Gradient", gradient)
         key = cv2.waitKey(33)
+        
+        if key == ord('q'): max_gray += 5
+        if key == ord('a'): max_gray -= 5
+        print(max_gray)
         
     cv2.destroyAllWindows()
 
