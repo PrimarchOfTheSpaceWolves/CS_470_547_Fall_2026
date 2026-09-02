7034caff051492b940181cbe5e9f385ae1c99145
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-02 15:30:31 -0400

    fix: prevent over/underflow on gradient

--- ProfExercise02.py
+++ ProfExercise02.py
@@ -4,6 +4,7 @@ import numpy as np
 def main():
     
     gradient = np.zeros((480,640), dtype="uint8")
+    float_gradient = np.zeros(gradient.shape, dtype="float64")
     
     key = -1
     ESC_KEY = 27
@@ -13,7 +14,9 @@ def main():
     while key != ESC_KEY:
         for i in range(gradient.shape[1]):
             fraction = i / (gradient.shape[1]-1)
-            gradient[:,i:(i+1)] = fraction*max_gray
+            float_gradient[:,i:(i+1)] = fraction*max_gray
+            
+        gradient = cv2.convertScaleAbs(float_gradient)
                     
         cv2.imshow("Gradient", gradient)
         key = cv2.waitKey(33)
