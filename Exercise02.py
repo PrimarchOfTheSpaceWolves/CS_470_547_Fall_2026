b4ae5b33eb214183c2b3538d28d2857390ae275d
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-02 14:55:37 -0400

    feat: add empty window

--- Exercise02.py
+++ Exercise02.py
@@ -1,6 +1,19 @@
+import cv2
+import numpy as np
 
 def main():
-    pass
+    
+    gradient = np.zeros((480,640), dtype="uint8")
+    
+    key = -1
+    ESC_KEY = 27
+    
+    while key != ESC_KEY:
+        
+        cv2.imshow("Gradient", gradient)
+        key = cv2.waitKey(33)
+        
+    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
