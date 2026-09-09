d06b23607df401c228ca9c0473097255b3f554d0
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-09 15:02:57 -0400

    feat: show gray sliced video image

--- ProfExercise02.py
+++ ProfExercise02.py
@@ -1,7 +1,15 @@
 import cv2
 import numpy as np
 
+def gray_slice(image, min_val, max_val):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    image = np.where(image < min_val, min_val, image)
+    image = np.where(image > max_val, max_val, image)
+    return image
+
 def main():
+    
+
     
     gradient = np.zeros((480,640), dtype="uint8")
     float_gradient = np.zeros(gradient.shape, dtype="float64")
@@ -15,7 +23,28 @@ def main():
     
     max_gray = 100
     
+    capture = cv2.VideoCapture("images/noice.mp4")
+    
+    if not capture.isOpened():
+        print("Could not open video!")
+        exit(1)
+    
+    min_slice = 100
+    max_slice = 200
+    
     while key != ESC_KEY:
+        
+        _, frame = capture.read()
+        
+        frame_cnt = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
+        frame_index = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
+        if frame_cnt != -1 and frame_cnt == frame_index:
+            capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            
+        cv2.imshow("Original Video", frame)
+        sliced_frame = gray_slice(frame, min_slice, max_slice)
+        cv2.imshow("Sliced Video", sliced_frame)
+        
         test_contrast[:,:] = back_gray
         test_contrast[200:600,100:700] = fore_gray        
         
@@ -25,11 +54,18 @@ def main():
             
         gradient = cv2.convertScaleAbs(float_gradient)
                     
-        cv2.imshow("Gradient", gradient)
-        cv2.imshow("Float Gradient", float_gradient/255.0)
-        cv2.imshow("Contrast", test_contrast)
+        #cv2.imshow("Gradient", gradient)
+        #cv2.imshow("Float Gradient", float_gradient/255.0)
+        #cv2.imshow("Contrast", test_contrast)
         
         key = cv2.waitKey(33)
+        
+        if key == ord('r'): min_slice += 5
+        if key == ord('f'): min_slice -= 5
+        
+        if key == ord('t'): max_slice += 5
+        if key == ord('g'): max_slice -= 5
+        print(min_slice, max_slice)
         
         if key == ord('w'): fore_gray = np.clip(fore_gray+1, 0, 255)
         if key == ord('s'): fore_gray = np.clip(fore_gray-1, 0, 255)
@@ -37,7 +73,7 @@ def main():
         if key == ord('e'): back_gray = np.clip(back_gray+1, 0, 255)
         if key == ord('d'): back_gray = np.clip(back_gray-1, 0, 255)
         
-        print(fore_gray, back_gray)
+        #print(fore_gray, back_gray)
         
         if key == ord('q'): max_gray += 5
         if key == ord('a'): max_gray -= 5
