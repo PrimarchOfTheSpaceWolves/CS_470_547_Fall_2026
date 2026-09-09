9d023f5d9495b01a63ed8c0ebc743d3b003e19ad
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-09 15:39:15 -0400

    feat: add down and up sampling

--- ProfExercise02.py
+++ ProfExercise02.py
@@ -32,9 +32,19 @@ def main():
     min_slice = 100
     max_slice = 200
     
+    scale = 1
+    
     while key != ESC_KEY:
         
         _, frame = capture.read()
+        
+        resized_frame = cv2.resize(frame, dsize=None, 
+                                   fx=1.0/scale, fy=1.0/scale,
+                                   interpolation=cv2.INTER_LINEAR)
+        resized_frame = cv2.resize(resized_frame, dsize=None,
+                                   fx=scale, fy=scale,
+                                   interpolation=cv2.INTER_NEAREST)
+        cv2.imshow("MY EYES", resized_frame)
         
         frame_cnt = int(capture.get(cv2.CAP_PROP_FRAME_COUNT))
         frame_index = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
@@ -60,12 +70,16 @@ def main():
         
         key = cv2.waitKey(33)
         
+        if key == ord('y'): scale += 1
+        if key == ord('h'): scale = max(1, scale-1)
+        print(scale)
+        
         if key == ord('r'): min_slice += 5
         if key == ord('f'): min_slice -= 5
         
         if key == ord('t'): max_slice += 5
         if key == ord('g'): max_slice -= 5
-        print(min_slice, max_slice)
+        #print(min_slice, max_slice)
         
         if key == ord('w'): fore_gray = np.clip(fore_gray+1, 0, 255)
         if key == ord('s'): fore_gray = np.clip(fore_gray-1, 0, 255)
