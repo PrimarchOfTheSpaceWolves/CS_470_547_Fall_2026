6ff60a4e51408a1a93e3c00a675bf9cde815a072
Dr. Michael J. Reale <realemj@sunypoly.edu>
2026-09-23 15:21:43 -0400

    feat: add imports

--- ProfExercise04_CIFAR10.py
+++ ProfExercise04_CIFAR10.py
@@ -1,3 +1,10 @@
+import torch
+from torch.utils.data import Dataset,DataLoader
+import torchvision
+from torchvision import datasets
+from torchvision.transforms import v2
+import cv2
+import numpy as np
 
 def main():
     pass
